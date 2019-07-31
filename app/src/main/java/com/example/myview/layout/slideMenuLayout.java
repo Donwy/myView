fbb59722069e2eba332c0f831b18b472f58eff40
Donvy_y <572419921@qq.com>
2019-07-31 17:01:48 +0800

    注销注释

--- app/src/main/java/com/example/myview/layout/slideMenuLayout.java
+++ app/src/main/java/com/example/myview/layout/slideMenuLayout.java
@@ -30,24 +30,24 @@ public class slideMenuLayout extends LinearLayout {
             case MotionEvent.ACTION_DOWN:
                 startX = event.getX();                    //鼠标点击（按下）事件获取的x坐标
                 startY = event.getY();
-                Log.d(TAG, "onTouchEvent---down事件获取的mouse的初始坐标: （" + startX + ", " + startY + ")");
+//                Log.d(TAG, "onTouchEvent---down事件获取的mouse的初始坐标: （" + startX + ", " + startY + ")");
                 break;
             case MotionEvent.ACTION_MOVE:
 
-                Log.d(TAG, "onTouchEvent---move1事件获取的mouse的mx、my坐标: （" + event.getX() + ",  " + event.getY() + ") ");
+//                Log.d(TAG, "onTouchEvent---move1事件获取的mouse的mx、my坐标: （" + event.getX() + ",  " + event.getY() + ") ");
                 final float mX = (int) (event.getX() - startX);              //鼠标在X轴的滑动距离
                 final float mY = (int) (event.getY() - startY);
 
                 int disX = (int) (getScrollX() - mX);
                 int disY = (int) (getScrollY() - mY);
-                Log.d(TAG, "onTouchEvent---move2事件获取的mouse的滑动位移mX: " + mX + " ,mY: " + mY);
-                Log.d(TAG, "onTouchEvent---move3事件获取的子控件view未滑动时的坐标: （" + getScrollX() + ", " + getScrollY() + ")");
-                Log.d(TAG, "onTouchEvent---move4事件获取的子控件view需要水平滑动的位移disX: " + disX +", disY"+ disY);
+//                Log.d(TAG, "onTouchEvent---move2事件获取的mouse的滑动位移mX: " + mX + " ,mY: " + mY);
+//                Log.d(TAG, "onTouchEvent---move3事件获取的子控件view未滑动时的坐标: （" + getScrollX() + ", " + getScrollY() + ")");
+//                Log.d(TAG, "onTouchEvent---move4事件获取的子控件view需要水平滑动的位移disX: " + disX +", disY"+ disY);
 
                 scrollTo(disX, disY);
-
-                Log.d(TAG, "onTouchEvent---move5事件获取的滑动后getScrollX: " + getScrollX() + ", getScrollY:" + getScrollY());
-                Log.d(TAG, "onTouchEvent---move6事件获取的子控件view已经滑动后实际坐标: （" + (-getScrollX()) + ", " + (-getScrollY()) + ")");
+//
+//                Log.d(TAG, "onTouchEvent---move5事件获取的滑动后getScrollX: " + getScrollX() + ", getScrollY:" + getScrollY());
+//                Log.d(TAG, "onTouchEvent---move6事件获取的子控件view已经滑动后实际坐标: （" + (-getScrollX()) + ", " + (-getScrollY()) + ")");
 
                 //重新设置初始坐标，把一次较长的滑动事件当作若干个小的滑动事件相加，每一次小滑动结束后的位置便是下一次小滑动事件开始的位置
                 startX = event.getX();
